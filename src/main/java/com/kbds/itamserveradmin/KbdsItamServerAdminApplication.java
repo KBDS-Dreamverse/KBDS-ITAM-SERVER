8449cd1e70a0ddef4fed165e712e91f2dbd6e71d
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-02-05 14:51:50 +0900

    init project

---
+++ src/main/java/com/kbds/itamserveradmin/KbdsItamServerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.kbds.itamserveradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbdsItamServerAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbdsItamServerAdminApplication.class, args);
+	}
+
+}
