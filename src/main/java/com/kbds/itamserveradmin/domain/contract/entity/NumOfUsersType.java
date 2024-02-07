82e1a074d42367a710d45ccee8ef449cfe9c3c9f
UU_jeong <80961726+oU-Ua@users.noreply.github.com>
2024-02-07 20:23:07 +0900

    chore : directory 수정

---
+++ src/main/java/com/kbds/itamserveradmin/domain/contract/entity/NumOfUsersType.java
@@ -0,0 +1,32 @@
+package com.kbds.itamserveradmin.domain.contract.entity;
+import com.kbds.itamserveradmin.domain.contract.entity.Contract;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NumOfUsersType {
+
+    @Id
+    private String NumOfUsersId;
+
+    @OneToOne
+    @JoinColumn(name = "cont_id")
+    private Contract cont;
+
+    private Integer maxUsersLimit;
+    private Integer maxCpuLimit;
+    private String ipRange;
+    private Integer currUsers;
+    private Integer currCpu;
+    private Integer totalNumPur;
+    private Integer totalServer;
+    private Integer totalCal;
+}
