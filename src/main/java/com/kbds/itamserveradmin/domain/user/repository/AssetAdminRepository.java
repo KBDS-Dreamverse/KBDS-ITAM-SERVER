ac732ab34eef2cacf1edbfe6cbf39d2a5088f390
UU_jeong <80961726+oU-Ua@users.noreply.github.com>
2024-02-07 21:23:09 +0900

    feat : user, purchase Repository 설계

---
+++ src/main/java/com/kbds/itamserveradmin/domain/user/repository/AssetAdminRepository.java
@@ -0,0 +1,7 @@
+package com.kbds.itamserveradmin.domain.user.repository;
+
+import com.kbds.itamserveradmin.domain.user.entity.AssetAdmin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssetAdminRepository extends JpaRepository<AssetAdmin,String> {
+}
