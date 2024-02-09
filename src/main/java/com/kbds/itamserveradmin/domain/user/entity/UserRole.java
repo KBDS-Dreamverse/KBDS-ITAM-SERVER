51912e7bf8f8b6ecffaed21fb0d14f174896eba2
oU-Ua <imuu30071126@gmail.com>
2024-02-10 00:43:58 +0900

    chore : UserRole 수정

--- src/main/java/com/kbds/itamserveradmin/domain/user/entity/UserRole.java
+++ src/main/java/com/kbds/itamserveradmin/domain/user/entity/UserRole.java
@@ -6,5 +6,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum UserRole {
-    L0,L1,L2,L3,L4;
+    L0("L0","주임"),
+    L1("L1","대리"),
+    L2_1("L2","과장"),L2_2("L2","차장"),
+    L3_1("L3","수석 차장"),L3_2("L3","팀장"),
+    L4_1("L4","부장"),L4_2("L4","본부장");
+    private final String code;
+    private final String value;
+
 }
